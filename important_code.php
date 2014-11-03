ed2b7f24712ff3497639ede81277c0199e540dc6
Daniel Billotte <daniel@litfuze.com>
2014-11-03 13:45:41 -0700

    added important file

---
+++ important_code.php
@@ -0,0 +1,12 @@
+<?php 
+
+// connect to db here
+$mysqli = new mysqli($host, $user, $pword, $db);
+
+// now do important stuff
+$query = "get my data";
+$results = $mysqli->query($query); 
+
+
+
+ ?>
